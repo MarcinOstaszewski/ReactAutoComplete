1deb3430be0f794dd973685e4455dbf083ab4f2c
MarcinOstaszewski <marcinostaszewski@gmail.com>
2018-01-27 12:38:26 +0100

    add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+require('es6-promise').polyfill();
+
+module.exports = {
+    // entry: ['./src/js/main.jsx'],
+    entry: {
+        'js/app.js': './src/js/Main.jsx'
+    },
+
+    output: {
+        filename: '[name]',
+        path: __dirname
+    },
+
+    plugins: [
+        new ExtractTextPlugin('./css/app.css')
+    ],
+
+    module: {
+        loaders: [{
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: { presets: ['es2015', 'stage-2', 'react'] }
+            },
+            {
+                test: /\.scss$/,
+                loader: "style-loader!css-loader?url=false!sass-loader"
+            },
+            {
+                test: /\.(png|jpg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[path][name].[ext]'
+                }
+            },
+        ],
+
+    },
+
+    stats: {
+        // Colored output
+        colors: true
+    },
+
+    // Create Sourcemaps for the bundle
+    devtool: 'source-map'
+};

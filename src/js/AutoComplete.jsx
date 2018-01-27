4cbc4e900cff4b7e8af5de8e7f16ae3411a6ca78
MarcinOstaszewski <marcinostaszewski@gmail.com>
2018-01-27 13:52:38 +0100

    update AutoComplete.js - add basic document structure

--- src/js/AutoComplete.jsx
+++ src/js/AutoComplete.jsx
@@ -9,15 +9,40 @@ document.addEventListener('DOMContentLoaded', function() {
 		constructor(props){
 			super(props);
 			this.state={
-				
+				autoCompleteText : "",
+				countries : []
 			}			
 		}	
 		
+		handleChange = (event) => {
+			this.setState({ autoCompleteText: event.target.value });
+		}
+
+		validateCountry = () => {
+			return true;
+		}
 
 		render() {
 			return (
 				<div className="mainPage">
-					<h1>Hello, World!</h1>
+					<div className="container">
+						<h1 className="title">
+							Mój pierwszy AutoComplete w ReactJS
+						</h1>
+						<p className="description">
+							Podaj nazwę państwa:
+						</p>
+						<input
+							required
+							className="countryInput"
+							name="country"
+							type="text"
+							value={this.state.autoCompleteText}
+							onChange={this.handleChange}
+							onBlur={this.validateCountry}
+						/>
+						<div className="prompt"></div>
+					</div>
 				</div>
 			)
 		}

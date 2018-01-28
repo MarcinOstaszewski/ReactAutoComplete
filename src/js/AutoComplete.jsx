0b7b6638193555c4222f037fa6d42cce90447616
MarcinOstaszewski <marcinostaszewski@gmail.com>
2018-01-28 18:37:50 +0100

    update AutoComplet.js - add more interactivity

--- src/js/AutoComplete.jsx
+++ src/js/AutoComplete.jsx
@@ -10,7 +10,8 @@ document.addEventListener('DOMContentLoaded', function() {
 			super(props);
 			this.state={
 				autoCompleteText : "",
-				countries : []
+				countriesWithIds : [],
+				countryNames: []
 			}			
 		}	
 		
@@ -18,69 +19,118 @@ document.addEventListener('DOMContentLoaded', function() {
 			this.setState({ autoCompleteText: event.target.value });
 		}
 
-		validateCountry = () => {
-			return true;
+		promptItemClicked = (event) => {
+			this.setState({autoCompleteText: event.currentTarget.id});
+		}
+
+		validateCountry = (event) => {
+			event.preventDefault();
 			// !!! check if value is on the list and save it OR CLEAR THE TEXT !!!
+			if (this.state.countryNames.includes( this.state.autoCompleteText )) {
+				console.log("Wybrano państwo: ", this.state.autoCompleteText)
+			} else {
+				this.setState({
+					autoCompleteText : ""
+				});
+				console.log("Nie ma takiego państwa");
+			}
+			return true;
+		}
+
+		createPromptList = (inputArray) => {
+			let promptList = inputArray.map( (item, index) => {
+				return ( 
+					<li key={index} className="promptList"> 
+						<div className="listItem" onClick={this.promptItemClicked} id={item[0]}>
+							<span className="leftAlign">{item[0]}</span> 
+							<span className="rightAlign">( {item[1]} )</span>
+						</div>
+					</li>
+				);
+			});
+			return promptList;
 		}
 
 		componentDidMount() {
 
 			let countriesUrl = 'http://vocab.nic.in/rest.php/country/json';
-			let tempCountries = [];
+			let tempCountriesWithIds = [],
+				tempCountryNames = [];
 
 			fetch(countriesUrl).then( r => r.json() ).then( response => {
-				response = response.countries;  // gets rid of an enclosing key
+				response = response.countries;  // gets rid of an enclosing object key
 				response.forEach( item => {
-					tempCountries.push(item.country)
-				}) // returns an array of objects { country_id: "...", country_name: "..." }
-
+					tempCountriesWithIds.push(item.country);
+					tempCountryNames.push(item.country["country_name"]);
+				}); // creates an array of objects { country_id: "...", country_name: "..." } and an array of country names
 
 				this.setState({
-					countries : tempCountries
-				})
+					countriesWithIds : tempCountriesWithIds,
+					countryNames : tempCountryNames.sort()
+				});
 			});
 		}
 
 		render() {
 
-			let filteredCountryNamesArray = [];
-			this.state.countries.forEach( item => {
-				if (item["country_name"].includes(this.state.autoCompleteText.toUpperCase())) {
+			let filteredCountryNamesArray = [],
+				filteredCountryIdsArray = [];
+			this.state.countriesWithIds.forEach( item => {
+				if (item["country_name"].slice(0,this.state.autoCompleteText.length) == this.state.autoCompleteText.toUpperCase()) {
 					filteredCountryNamesArray.push([item["country_name"], item["country_id"]]);
 				}
-			});			
-			console.log(filteredCountryNamesArray);
-
-			let countryPrompt = filteredCountryNamesArray.map( (item, index) => {
-				return ( 
-					<li key={index} className="promptList"> 
-						<span className="leftAlign">{item[1]} </span> 
-						<span className="rightAlign"> {item[0]}</span>
-					</li>
-				);
-			});
-
+				if (this.state.autoCompleteText.length >= 3) { 
+					filteredCountryIdsArray = []; // delete this list if input longer then ID
+				} else {
+					if (item["country_id"].slice(0,this.state.autoCompleteText.length) == this.state.autoCompleteText.toUpperCase()) {
+						filteredCountryIdsArray.push([item["country_name"], item["country_id"]]);
+					}
+				}
+				});		
+			filteredCountryNamesArray.sort();
+			filteredCountryIdsArray.sort();
+			
+			let idsList = this.createPromptList(filteredCountryIdsArray);
+			let namesList = this.createPromptList(filteredCountryNamesArray);
+			let divider = (
+				<li key="0000" className="promptList"> 
+					<div className="listItem">
+						<span></span>
+						<span className="divider">
+							- - - - - matchings found in country code - - - - -
+						</span>
+						<span></span>
+					</div>
+				</li>
+			);
+			
 			return (
 				<div className="mainPage">
 					<div className="container">
 						<h1 className="title">
-							Mój pierwszy AutoComplete w ReactJS
+							My first ReactJS AutoComplete Component
 						</h1>
 						<p className="description">
-							Podaj nazwę państwa:
+							Start typing a country name or ID:
 						</p>
-						<input
-							required
-							className="countryInput"
-							name="country"
-							type="text"
-							value={this.state.autoCompleteText}
-							onChange={this.handleChange}
-							onBlur={this.validateCountry}
-						/>
-						<ul className="countryPrompt">
-							{this.state.autoCompleteText != "" ? countryPrompt : null }
-						</ul>
+						<form onSubmit={this.validateCountry}>
+							<input
+								required
+								autoFocus="true"
+								autoComplete="off"
+								className="countryInput"
+								name="country"
+								type="text"
+								value={this.state.autoCompleteText}
+								onChange={this.handleChange}
+								// onBlur={this.validateCountry}
+							/>
+							<ul className="countryPrompt">
+								{this.state.autoCompleteText != "" ? namesList : null }
+								{this.state.autoCompleteText != "" ? divider : null }
+								{this.state.autoCompleteText.length > 0 && this.state.autoCompleteText.length < 3 ? idsList : null }
+							</ul>
+						</form>
 					</div>
 				</div>
 			)
